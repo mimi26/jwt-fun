2c19a1aa406caaca89de205da1e89e4e3387746c
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-03-14 19:55:59 -0400

    add order controller

---
+++ services/order.js
@@ -0,0 +1,9 @@
+const Orders = require('../models').Order;
+
+const getAll = () => Orders.findAll();
+
+const getById = id => Orders.findById(id);
+
+const add = order => Orders.create(order);
+
+module.exports = { add, getAll, getById }
