e5d0c0d42aa31cfa696d15654360f2f6f461ba6b
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-03-14 20:00:02 -0400

    add order routes

--- router.js
+++ router.js
@@ -1,6 +1,11 @@
 const authController = require('./controllers/auth');
 
+const orderController = require('./controllers/order');
+
 module.exports.set = app => {
    app.post('/login', authController.login);
    app.post('/register', authController.register);
+   app.get('/orders', orderController.getOrders);
+   app.get('/orders/:id', orderController.getOrder);
+   app.post('/orders', orderController.addOrder);
 }
